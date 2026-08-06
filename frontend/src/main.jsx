74fccda2e30b4b96ee9315020c6c5dfd04af0771
Kelvin Joaquin <kelvin.joaquin@icloud.com>
2026-08-06 18:38:02 +0800

    feat: replace stylesheet with minimal-editorial base/home/case-study split

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/base.css'
+import './styles/home.css'
+import './styles/case-study.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
